e84872fd084a1e0dbd61d856fa772b228bdfa6a9
Newton <biinewton382@gmail.com>
2017-12-15 11:55:09 +0300

    Add navbar and landing page.

--- app/__init__.py
+++ app/__init__.py
@@ -35,11 +35,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     # setting conig
-    from .requests import configure_request
-    configure_request(app)
 
     return app
