cbec0e07eaebe2c7f8ed826b595a6f3899775aae
Newton <biinewton382@gmail.com>
2017-12-16 09:09:54 +0300

    Update mail server.

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export SECRET_KEY='this is real'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://newton:newton@localhost/pitch'
+export MAIL_USERNAME ='biinewtondev@yahoo.com'
+export MAIL_PASSWORD ='Moringa@123'
 
 python3.6 manage.py server
